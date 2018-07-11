80e8ad59a551ff691b5c6fdf275efc2aba2c6790
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-11 20:54:03 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,13 +2,13 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    if Time.now < 1200
+
+    if Time.now.hour < 12
       resp.write "Good Morning User"
     else
       resp.write "Good Afternoon User"
     end
     resp.finish
   end
-  puts = Time.now
 
 end
